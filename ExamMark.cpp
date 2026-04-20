502be6540962e2869b7b0106369cc73bda45cd98
Marcus Lewis <mlewis43@aum.edu>
2026-04-20 10:13:15 -0500

    Add Exam class to manage exam marks

---
+++ ExamMark.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Exam {
+private:
+    float marks;
+
+public:
+    void setMarks(float marks);
+    float getMarks() const;
+};
+
+void Exam::setMarks(float marks) {
+    this->marks = marks;
+}
+
+float Exam::getMarks() const {
+    return marks;
+}
+
+int main() {
+    Exam my_exam;
+    float input_marks;
+
+    cout << "Enter marks: ";
+    cin >> input_marks;
+
+    my_exam.setMarks(input_marks);
+    cout << "Marks: " << my_exam.getMarks() << endl;
+
+    return 0;
+}
