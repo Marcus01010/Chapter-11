a26237c149951abeec52d971c9ea8d9471d52f31
Marcus Lewis <mlewis43@aum.edu>
2026-04-20 10:16:54 -0500

    Create BusNumber.cpp

---
+++ BusNumber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class BusSeat
+{
+private:
+    int seat_number;
+
+public:
+    BusSeat(int seat);
+    int getSeatNumber() const;
+};
+
+BusSeat::BusSeat(int seat)
+{
+    seat_number = seat;
+}
+
+int BusSeat::getSeatNumber() const
+{
+    return seat_number;
+}
+
+int main()
+{
+    BusSeat my_seat(12);
+    cout << "Seat Number: " << my_seat.getSeatNumber() << endl;
+    return 0;
+}
